d0faef715b55f7311b7d5a5a2258cce9ac771b78
yuuki kurose <pw1006@icloud.com>
2022-08-27 20:28:06 +0900

    feat: searchコンポーネント生成、セレクタの差し込み
    refactor: インデント修正
    
    検索用のコンポーネントを生成しました。
    各編集するファイルのインデント修正を行いました。
    表示する為、app.component.htmlにセレクタを差し込みました。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }         from '@angular/core';
+import { BrowserModule }    from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent }     from './app.component';
+import { SearchComponent }  from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
